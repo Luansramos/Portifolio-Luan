22123ef66b3710dc79b6bfa945a9c1151302b62c
luansramos <luanramosgarciadesouza014@gmail.com>
2024-10-02 20:16:25 -0300

    terminando home

--- portifolio/pages/index.js
+++ portifolio/pages/index.js
@@ -13,7 +13,7 @@ import { fadeIn } from '../variants'
 
 const Home = () => {
   return (
-// texto
+    // texto
     <div className="bg-primary/60 h-full " >
 
 
@@ -23,47 +23,54 @@ const Home = () => {
 
           {/* //Titulo */}
           <motion.h1
-          variants={fadeIn('down',0.2)} initial="hidden" 
-          animate='show'
-          exit={'hidden'}
-          
-          className="h1" > transformando ideias <br /> Into{''}
+            variants={fadeIn('down', 0.2)} initial="hidden"
+            animate='show'
+            exit={'hidden'}
+
+            className="h1" > transformando ideias <br /> Into{''}
             <span className="text-accent"> Digital Realist</span>
           </motion.h1>
 
           {/* //subtitulo */}
-          <motion.p 
-          variants={fadeIn('down',0.3)} initial="hidden" 
-          animate='show'
-          exit={'hidden'}
-          className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">lorem aaaaaaaaaaaaaa aaaaaaaaaaaaaa aaaa aaaa aaaaaa aaa aaa aaaa a</motion.p>
+          <motion.p
+            variants={fadeIn('down', 0.3)} initial="hidden"
+            animate='show'
+            exit={'hidden'}
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">lorem aaaaaaaaaaaaaa aaaaaaaaaaaaaa aaaa aaaa aaaaaa aaa aaa aaaa a</motion.p>
 
-          <div className="flex justify-center xl:hidden relative">  
-            <ProjectsBtn/>
+          <div className="flex justify-center xl:hidden relative">
+            <ProjectsBtn />
           </div>
-          <motion.div 
-          variants={fadeIn('down',0.4)} initial="hidden" 
-          animate='show'
-          exit={'hidden'}
-          className="hidden xl:flex"
-          
-          > 
-            
-            <ProjectsBtn/>
-          
+          <motion.div
+            variants={fadeIn('down', 0.4)}
+            initial="hidden"
+            animate='show'
+            exit={'hidden'}
+            className="hidden xl:flex"
+
+          >
+
+            <ProjectsBtn />
+
           </motion.div>
 
 
         </div>
       </div>
-      {/* //imagem  */} <div className="w-[1200px] h-full absolute right-0 bottom-0"> 
-      {/* bg imagem */} <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge transition-z-0"></div>
-      {/* particulas */} <div className="">
-        particulas
-      </div>
-      {/* avatar */} <div>
-        <Avatar/>
-      </div>
+      {/* //imagem  */} <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        {/* bg imagem */} <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge transition-z-0"></div>
+        {/* particulas */} <div className="">
+          particulas
+        </div>
+        {/* avatar */} <motion.div
+          variants={fadeIn('up', 0.5)}
+          initial="hidden"
+          animate='show'
+          exit={'hidden'}
+          transition={{duration:1, ease: 'easeInOut'}}
+          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
+          <Avatar />
+        </motion.div>
       </div>
 
     </div>
