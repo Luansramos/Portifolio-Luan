8aec7fd8dd18ce95639dfbe320c33e9711d9d7ae
luansramos <luanramosgarciadesouza014@gmail.com>
2024-10-02 08:33:25 -0300

    fazendo Nav

--- portifolio/components/Nav.js
+++ portifolio/components/Nav.js
@@ -26,8 +26,90 @@ export const navData = [
   },
 ];
 
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 const Nav = () => {
-  return <nav>nav</nav>;
+  const router = useRouter();
+  const pathname = router.pathname;
+
+
+  return (
+
+    <nav className='
+    flex 
+    flex-col 
+    items-center 
+    xl:justify-center 
+    gap-y-4 
+    fixed 
+    h-max 
+    bottom-0 
+    mt-auto 
+    xl:right-[2%] 
+    z-50 
+    top-0 
+    w-full 
+    xl:w-16 
+    xl:max-w-md 
+    xl:h-screen 
+    
+    '>
+      {/*inner*/}
+      <div className='
+        flex
+        w-full 
+        xl:flex-col
+        items-center
+        justify-between
+        xl:justify-center
+        gap-y-10
+        px-4
+        md:px-40 
+        xl:px-0 
+        h-[80px]
+       
+        xl:h-max 
+        py-8 
+        bg-white/10 
+        backdrop-blur-sm 
+        text-3xl 
+        xl:text-xl 
+        xl:rounded-full'>
+        {navData.map((link, index) => {
+          return (
+            <div>
+
+              <Link className={`${link.path === pathname && 'text-accent'} relative flex items-center group hover:text-accent transition-all duration-300 `}
+                href={link.path}
+                key={index}
+              >
+                {/* descriçao ao lado do icon */}
+                <div className='absolute pr-14 right-0 hidden xl:group-hover:flex'>
+                  <div className='bg-white relative flex text-primary items-center p-[6px] rounded-[3px]'>
+                    <div className='text-[12px] leading-none font-semibold capitalize'> {link.name} </div>
+                  </div>
+                </div>
+
+                <div className='border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2'>
+                  
+                </div>
+
+
+                {/* icon */ }
+          <div>
+            {link.icon}
+          </div>
+              </Link>
+
+    </div>
+
+  );
+
+})}
+      </div >
+    </nav >
+  );
 };
 
 export default Nav;
