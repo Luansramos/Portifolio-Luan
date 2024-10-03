96d7d5334391b2652db7867a84eac1e75fda7d19
luansramos <luanramosgarciadesouza014@gmail.com>
2024-10-03 16:10:36 -0300

    colocando meu logo

--- portifolio/components/Header.js
+++ portifolio/components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
       <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
         {/* logo */}
         <Link href={'/'}>
-          <Image src={'/logo.svg'}
+          <Image src={'/logosemfundo.png'}
             width={220}
             height={48}
             alt=""
